9ad55e5885dd9d5f0aacb2c3053ee8fbcf4edbac
Peter East <me@peterea.st>
2021-03-20 17:05:27 +0000

    Define the object model

---
+++ app/src/main/kotlin/scopify/work/backend/model/Model.kt
@@ -0,0 +1,36 @@
+package scopify.work.backend.model
+
+import kotlin.random.Random
+
+enum class ScopeState {
+    Proposed,
+    InProgress,
+    Complete
+}
+
+data class ScopeScore(
+        val name: String,
+        val value: Int,
+        val comment: String?
+)
+
+// A scoping session to be defined
+data class ScopeSession(
+        val id: String,
+        val title: String,
+        val description: String,
+        val scores: List<ScopeScore> = listOf(),
+        val averageScore: Double = 0.0,
+        var state: ScopeState = ScopeState.Proposed
+)
+
+interface IScopeRepository {
+    fun getSession(id: String): ScopeSession?
+    fun writeSession(session: ScopeSession)
+}
+
+private val charPool: List<Char> = ('a'..'z') + ('A'..'Z')
+fun generateId(len: Int = 10) = (0..len)
+        .map { Random.nextInt(0, charPool.size) }
+        .map ( charPool::get )
+        .joinToString("")
