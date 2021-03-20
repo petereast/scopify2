c8ce2c92769ea5d72431be8e5ce1accb135f7699
Peter East <me@peterea.st>
2021-03-20 17:06:45 +0000

    Add http service

---
+++ backend/app/src/main/kotlin/scopify/work/backend/graphql/Schema.kt
@@ -0,0 +1,43 @@
+package scopify.work.backend.graphql
+
+import com.apurebase.kgraphql.schema.dsl.SchemaBuilder
+import scopify.work.backend.infrastructure.RedisDal
+import scopify.work.backend.model.ScopeScore
+import scopify.work.backend.model.ScopeSession
+import scopify.work.backend.model.ScopeState
+import scopify.work.backend.service.Scopify
+
+fun SchemaBuilder.scopeSchema() {
+    val sessionService = Scopify(RedisDal())
+
+    query("session") {
+        description = "Gets a single scoping session"
+        resolver { id: String ->
+            sessionService.getById(id)
+        }
+    }
+
+    type<ScopeSession>()
+    enum<ScopeState>()
+
+    mutation("createSession") {
+        description = "Creates a new session"
+
+        resolver { title: String, description: String ->
+            sessionService.createSession(title, description)
+        }
+    }
+
+    mutation("submitScore") {
+        description = "Submits a score to a soping session"
+
+        resolver { sessionId: String, name: String, score: Int ->
+            sessionService.addScope(sessionId, ScopeScore(name, score, null))
+        }
+    }
+
+    mutation("finishSession") {
+        description = "Ends a session. The session will no longer accept new scopes"
+        resolver { sessionId: String -> sessionService.finishSession(sessionId) }
+    }
+}
