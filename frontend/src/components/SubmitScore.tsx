6fe79a040f5215f2606d375ba7126f05f31aa7a1
Peter East <me@peterea.st>
2021-03-21 21:01:15 +0000

    Update package.json properly and add some more frontend

--- frontend/src/components/SubmitScore.tsx
+++ frontend/src/components/SubmitScore.tsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
 
-export default function SubmitScore() {
+const SUBMIT_SCOPE_SCORE = gql`
+  mutation SubmitScore($id: String!, score: Int!, name: String!){
+    submitScore(sessionId: $id, score: $score, name: $name) {
+      __typename
+      id
+      averageScore
+      scores {
+        name
+        score
+      }
+    }
+  }
+`;
+
+export default function SubmitScore({ id }: { id: string }) {
+  const [userName, setUserName] = useState(
+    localStorage.getItem("users_name") || ""
+  );
+
+  const [score, setScore] = useState(0);
+
+  const [submitScopeScore, { error, loading, data }] = useMutation(
+    SUBMIT_SCOPE_SCORE,
+    {
+      variables: {
+        id,
+        name: userName,
+        score: score,
+      },
+    }
+  );
+
   const radioButton = (value: string | number) => (
-              <label className="radio notification" key={value}>
-                <input className="radio" type="radio" name="something"/>
-                <span className="ml-2 is-size-4">{value}</span>
-              </label>)
+    <label className="radio notification" key={value}>
+      <input className="radio" type="radio" name="something" />
+      <span className="ml-2 is-size-4">{value}</span>
+    </label>
+  );
+
   return (
     <div className="card">
       <div className="card-content">
@@ -21,18 +55,22 @@ export default function SubmitScore() {
                 id="yourName"
                 placeholder="Hi, my name is ______"
                 className="input"
+                value={userName}
+                onChange={(ev) => setUserName(ev.target.value)}
               />
             </div>
           </div>
           <div className="mt-5 block field">
             <label className="label">And I think this task is worth...</label>
             <div className="control">
-              {[1,2,3,5,8,13].map(radioButton)}
+              {[1, 2, 3, 5, 8, 13].map(radioButton)}
             </div>
           </div>
           <div className="block field">
             <div className="control">
-              <button type="submit" className="button is-success">Scope it!</button>
+              <button type="submit" className="button is-success">
+                Scope it!
+              </button>
             </div>
           </div>
         </form>
