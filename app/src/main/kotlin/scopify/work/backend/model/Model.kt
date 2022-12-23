b9f6d824f0c34acabb7df6a4aa64202d017f9b94
Peter East <me@peterea.st>
2022-12-23 17:38:23 +0000

    Move remove redundant backend

--- app/src/main/kotlin/scopify/work/backend/model/Model.kt
+++ app/src/main/kotlin/scopify/work/backend/model/Model.kt
@@ -9,28 +9,38 @@ enum class ScopeState {
 }
 
 data class ScopeScore(
-        val name: String,
-        val value: Int,
-        val comment: String?
+    val name: String,
+    val value: Int,
+    val comment: String?
 )
 
 // A scoping session to be defined
 data class ScopeSession(
-        val id: String,
-        val title: String,
-        val description: String,
-        val scores: List<ScopeScore> = listOf(),
-        val averageScore: Double = 0.0,
-        var state: ScopeState = ScopeState.Proposed
+    val id: String,
+    val title: String,
+    val description: String,
+    val group: String?,
+    val scores: List<ScopeScore> = listOf(),
+    val averageScore: Double = 0.0,
+    var state: ScopeState = ScopeState.Proposed
+)
+
+data class ScopeGroup(
+    val id: String,
+    val title: String
 )
 
 interface IScopeRepository {
+    fun createGroup(id: String, title: String): ScopeGroup?
+    fun getGroupScopes(id: String): List<String>
+    fun getGroup(id: String): ScopeGroup?
     fun getSession(id: String): ScopeSession?
     fun writeSession(session: ScopeSession)
 }
 
 private val charPool: List<Char> = ('a'..'z') + ('A'..'Z')
-fun generateId(len: Int = 5) = (0..len)
-        .map { Random.nextInt(0, charPool.size) }
-        .map ( charPool::get )
-        .joinToString("")
+
+fun generateId(len: Int = 5): String = (0..len)
+    .map { Random.nextInt(0, charPool.size) }
+    .map(charPool::get)
+    .joinToString("")
--- app/src/main/kotlin/scopify/work/backend/service/Scopify.kt
+++ app/src/main/kotlin/scopify/work/backend/service/Scopify.kt
@@ -5,7 +5,7 @@ import scopify.work.backend.model.*
 class SessionNotFound(id: String) : Exception("Session with it \"$id\" not found!")
 class SessionAlreadyFinished(id: String) : Exception("Session $id already finished!")
 
-class Scopify(val scopeRepo: IScopeRepository) {
+class Scopify(private val scopeRepo: IScopeRepository) {
 
     private fun calculateAverageScore(scores: List<ScopeScore>): Double {
         val totalScore: Int = scores.map { item -> item.value }.fold(0) { acc: Int, i: Int -> i + acc }
@@ -21,9 +21,10 @@ class Scopify(val scopeRepo: IScopeRepository) {
         }
     }
 
-    fun createSession(title: String, description: String): ScopeSession {
+    fun createSession(title: String, description: String, groupId: String? = null): ScopeSession {
+      
         try {
-            val scopeSession = ScopeSession(generateId(), title, description)
+            val scopeSession = ScopeSession(generateId(), title, description, group = groupId)
             scopeRepo.writeSession(scopeSession)
             return scopeSession
         } catch (e: Exception) {
@@ -46,12 +47,13 @@ class Scopify(val scopeRepo: IScopeRepository) {
                 scores.add(scopeScore)
 
                 val updatedSession = ScopeSession(
-                        id = existingSession.id,
-                        title = existingSession.title,
-                        description = existingSession.description,
-                        scores = scores,
-                        averageScore = calculateAverageScore(scores),
-                        state = ScopeState.InProgress
+                    id = existingSession.id,
+                    title = existingSession.title,
+                    description = existingSession.description,
+                    group = existingSession.group,
+                    scores = scores,
+                    averageScore = calculateAverageScore(scores),
+                    state = ScopeState.InProgress
                 )
                 scopeRepo.writeSession(updatedSession)
 
@@ -70,12 +72,25 @@ class Scopify(val scopeRepo: IScopeRepository) {
             val session = scopeRepo.getSession(sessionId) ?: throw SessionNotFound(sessionId)
             if (session.state == ScopeState.Complete) throw SessionAlreadyFinished(sessionId)
 
-            session.state = ScopeState.Complete
-            scopeRepo.writeSession(session)
+            val newSession = session.copy(state = ScopeState.Complete)
+
+            scopeRepo.writeSession(newSession)
 
             return session
         } catch (e: Exception) {
             throw e
         }
+    }
+
+    fun getScopeGroup(id: String): ScopeGroup? {
+      return scopeRepo.getGroup(id)
+    }
+
+    fun createScopeGroup(title: String): ScopeGroup {
+      return requireNotNull(scopeRepo.createGroup(generateId(), title))
+    }
+
+    fun getGroupScopes(id: String): List<String> {
+      return scopeRepo.getGroupScopes(id)
     }
 }
